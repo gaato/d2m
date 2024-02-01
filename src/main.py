28687da751396d5753ef89928908296040ae6e04
Gakuto Furuya <g.furuya@gaato.net>
2024-02-01 21:57:28 +0900

    `visibility="followers"`

--- src/main.py
+++ src/main.py
@@ -53,6 +53,7 @@ async def on_message(message: discord.Message):
             f = misskey.drive_files_create(
                 file=io.BytesIO(await attachment.read()),
                 name=attachment.filename,
+                visibility="followers",
             )
             file_ids.append(f["id"])
 
