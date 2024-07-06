daabfb10812cb2601c56bc86fa26cffffa009c24
gaato <g.furuya@gaato.net>
2024-07-06 18:46:51 +0900

    Fix a silly mistake

--- src/main.py
+++ src/main.py
@@ -53,7 +53,6 @@ async def on_message(message: discord.Message):
             f = misskey.drive_files_create(
                 file=io.BytesIO(await attachment.read()),
                 name=attachment.filename,
-                visibility="home",
             )
             file_ids.append(f["id"])
 
@@ -66,6 +65,7 @@ async def on_message(message: discord.Message):
     misskey.notes_create(
         text=text,
         file_ids=file_ids if file_ids else None,
+        visibility="home",
     )
 
 
